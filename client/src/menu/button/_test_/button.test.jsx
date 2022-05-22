b28511acf11d36cb99a3d891071d107aec61018b
Lizl van Jaarsveld <lizlvnjaarsveld@gmail.com>
2022-05-22 20:23:09 +0200

    trying to fix codecov attempt 11

--- client/src/menu/button/_test_/button.test.jsx
+++ client/src/menu/button/_test_/button.test.jsx
@@ -4,11 +4,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from "../button";
 import {isTSAnyKeyword} from "@babel/types";
+import Btn from '../button';
 
 
 test('use jsdom in this test file', () => {
     const element = document.createElement('div');
-    expect(<Button></Button>).not.toBeNull();
+    ReactDOM.render(<Btn></Btn>,element);
   });
