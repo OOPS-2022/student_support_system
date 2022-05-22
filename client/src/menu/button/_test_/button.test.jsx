0f44e18eaea5d90d2cb60105b111eee4411c452a
Lizl van Jaarsveld <lizlvnjaarsveld@gmail.com>
2022-05-22 22:14:20 +0200

    updating code cov

--- client/src/menu/button/_test_/button.test.jsx
+++ client/src/menu/button/_test_/button.test.jsx
@@ -3,12 +3,15 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {isTSAnyKeyword} from "@babel/types";
 import Btn from '../button';
 
 
 test('use jsdom in this test file', () => {
     const element = document.createElement('div');
-    ReactDOM.render(<Btn></Btn>,element);
+     let root = ReactDOM.createRoot(element)
+     root.render(<Btn></Btn>);
   });
+
+  
